66505f7dfe0036efae3f5ee960c9be9ec968c68e
marmulle <marmulle@student.hive.fi>
2024-08-10 13:22:30 +0300

    add exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,43 +1,4 @@
-const Header = ({ name }) => {
-	return <h1>{name}</h1>;
-};
-
-const Part = ({ part }) => {
-	return (
-		<p>
-			{part.name} {part.exercises}
-		</p>
-	);
-};
-
-const TotalParts = ({ parts }) => {
-	const total = parts.reduce((sum, part) => sum + part.exercises, 0);
-	return (
-		<p>
-			<b>total of {total} exercises</b>
-		</p>
-	);
-};
-
-const Content = ({ parts }) => {
-	return (
-		<div>
-			{parts.map((part) => (
-				<Part key={part.id} part={part} />
-			))}
-			<TotalParts parts={parts} />
-		</div>
-	);
-};
-
-const Course = ({ course }) => {
-	return (
-		<div>
-			<Header name={course.name} />
-			<Content key={course.id} parts={course.parts} />
-		</div>
-	);
-};
+import Course from "./components/Course";
 
 const App = () => {
 	const courses = [
