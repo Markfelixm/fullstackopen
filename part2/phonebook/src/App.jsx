a40fd2152cf54809e88d9b2c70cd74f574e841fb
marmulle <marmulle@student.hive.fi>
2024-08-10 14:49:20 +0300

    add exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,36 @@
 import { useState } from "react";
 
+const PersonLine = ({ person }) => <p>{person.name}</p>;
+
 const App = () => {
 	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 	const [newName, setNewName] = useState("");
 
+	const onNameChange = (event) => {
+		setNewName(event.target.value);
+	};
+
+	const addName = (event) => {
+		event.preventDefault();
+		setPersons([...persons].concat({ name: newName }));
+		setNewName("");
+	};
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={addName}>
 				<div>
-					name: <input />
+					name: <input value={newName} onChange={onNameChange} />
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			...
+			{persons.map((person) => (
+				<PersonLine key={person.name} person={person} />
+			))}
 		</div>
 	);
 };
