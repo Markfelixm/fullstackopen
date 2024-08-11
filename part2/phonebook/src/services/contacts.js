9cd8bf562d518ecf1fac5d3170efe9fab23311e7
marmulle <marmulle@student.hive.fi>
2024-08-11 14:44:29 +0300

    add (services): catch on promises

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -2,13 +2,23 @@ import axios from "axios";
 
 const baseURL = "http://localhost:3001/persons";
 
-const getAll = () => axios.get(baseURL).then((response) => response.data);
+const getAll = () =>
+	axios
+		.get(baseURL)
+		.then((response) => response.data)
+		.catch(() => console.log("could not get all"));
 
 const create = (newContact) =>
-	axios.post(baseURL, newContact).then((response) => response.data);
+	axios
+		.post(baseURL, newContact)
+		.then((response) => response.data)
+		.catch(() => console.log("could not create contact"));
 
 const remove = (id) =>
-	axios.delete(baseURL.concat(`/${id}`)).then((response) => response.data);
+	axios
+		.delete(baseURL.concat(`/${id}`))
+		.then((response) => response.data)
+		.catch(() => console.log("could not delete contact"));
 
 export default {
 	getAll,
