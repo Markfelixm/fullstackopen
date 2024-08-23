b937cafbc84ccf3722d94fbbf7ecb6bf624e2acd
marmulle <marmulle@student.hive.fi>
2024-08-23 14:37:34 +0300

    add module controllers/blogs

--- part4/index.js
+++ part4/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const middleware = require("./utils/middleware");
-const Blog = require("./models/blog");
+const blogRouter = require("./controllers/blogs");
 
 mongoose
 	.connect(config.mongoUrl)
@@ -20,19 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
-app.get("/api/blogs", (request, response) => {
-	Blog.find({}).then((blogs) => {
-		response.json(blogs);
-	});
-});
-
-app.post("/api/blogs", (request, response) => {
-	const blog = new Blog(request.body);
-
-	blog.save().then((result) => {
-		response.status(201).json(result);
-	});
-});
+app.use("/api/blogs", blogRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
