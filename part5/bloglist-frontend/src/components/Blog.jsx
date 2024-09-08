72f02227e0363a5427c1e62899b6cde5ae2c53ae
marmulle <marmulle@student.hive.fi>
2024-09-08 18:35:02 +0300

    add exercise 5.7; add blog detailed view

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,8 +1,49 @@
-const Blog = ({ blog }) => (
-	<div>
-		{blog.title}
-		{blog.author ? `, ${blog.author}` : ""}
-	</div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+	const [showDetails, setShowDetails] = useState(false);
+
+	const blogStyle = {
+		paddingTop: 10,
+		paddingLeft: 2,
+		border: "solid",
+		borderWidth: 1,
+		marginBottom: 5,
+	};
+
+	const ToggleButton = () => {
+		if (showDetails) {
+			return <button onClick={() => setShowDetails(false)}>hide</button>;
+		} else {
+			return <button onClick={() => setShowDetails(true)}>view</button>;
+		}
+	};
+
+	const base = () => (
+		<span>
+			{blog.title}
+			{blog.author ? `, ${blog.author}` : ""} <ToggleButton />
+		</span>
+	);
+
+	const details = () => (
+		<div>
+			<span>{blog.url}</span>
+			<br />
+			<span>
+				likes: {blog.likes} <button>like</button>
+			</span>
+			<br />
+			<span>{blog.user.name}</span>
+		</div>
+	);
+
+	return (
+		<div style={blogStyle}>
+			{base()}
+			{showDetails ? details() : ""}
+		</div>
+	);
+};
 
 export default Blog;
